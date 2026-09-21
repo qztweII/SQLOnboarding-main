09f7e93dcb204b29d425423d98ca02b740b1e457
qztweII <terence.wu@education.nsw.gov.au>
2026-09-21 14:31:55 +1000

    Added sorting by year group then name
    Also added custom search by year.

--- scripts/lesson4_filter_sort_summarize.py
+++ scripts/lesson4_filter_sort_summarize.py
@@ -6,13 +6,18 @@ database = sql.connect("school.db")
 cursor = database.cursor()
 
 year_number = 10
-cursor.execute("SELECT name, year_group FROM students WHERE year_group = ? ORDER BY name", (year_number, ))
+cursor.execute("SELECT name, year_group FROM students ORDER BY year_group DESC, name")
 
 group = cursor.fetchall()
 
 for i in group:
-    print(i)
+    print(f"{i[1]} : {i[0]}")
 
 cursor.execute("SELECT COUNT(*) FROM students")
 studentAmount = cursor.fetchone()[0]
 print("Students amount: ", studentAmount)
+
+year = int(input())
+cursor.execute("SELECT COUNT(*) FROM students WHERE year_group = ?", (year, ))
+year_amount = cursor.fetchone()[0]
+print(f"Amount of year {year}: {year_amount}")
